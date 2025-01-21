ce8784f1ed7ded57fe9b9ffb30c1320a8599a2e4
deveshbis <deveshbiswas.cse@gmail.com>
2025-01-21 11:26:47 +0600

    created tailwind setup and file structures

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <!-- <NuxtPage>
+      <NuxtLayout>
+
+      </NuxtLayout>
+    </NuxtPage> -->
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">Hello DD Chat App</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
