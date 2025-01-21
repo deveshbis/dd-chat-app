076c1801b9c7f574653bcda8d45fef2fef6acccd
deveshbis <deveshbiswas.cse@gmail.com>
2025-01-21 12:22:38 +0600

    added chat banner section

---
+++ components/Banner/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="font-sans">
+        <div class="grid lg:grid-cols-2 items-center lg:gap-y-6 bg-blue-500">
+            <div class="max-lg:order-1 max-lg:text-center sm:p-12 p-4">
+                <h2 class="text-gray-800 lg:text-5xl text-3xl font-bold lg:!leading-[56px]">Connect Instantly, Chat Seamlessly</h2>
+                <p class="text-gray-800 mt-6 text-base leading-relaxed">Experience the power of real-time communication with DD Chat App. Stay connected with friends, family, and colleagues through a modern, secure, and user-friendly platform. Whether it's personal conversations or professional collaborations, DD Chat App brings you closer to the people who matter most</p>
+                <button type='button'
+                    class="bg-transparent border-2 border-gray-800 mt-12 transition-all text-gray-800 font-bold text-sm rounded-md px-6 py-2.5">Get
+                    Started</button>
+            </div>
+
+            <div class="lg:h-[480px] flex items-center">
+                <img src="https://readymadeui.com/team-image.webp" class="w-full h-full object-cover"
+                    alt="Dining Experience" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
