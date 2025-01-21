43709d46369beba6d595ed755803ea4f9182f1d4
deveshbis <deveshbiswas.cse@gmail.com>
2025-01-21 15:53:18 +0600

    added chatbox

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,23 @@
 <template>
-<div>
-    <nav class="bg-gray-800 p-4">
-      <div class="container mx-auto flex justify-between items-center">
-        <div class="flex items-center">
-          <img src="/assets/logo.png" alt="Logo" class="h-8 mr-2" />
-          <span class="text-white text-xl font-semibold">DD Chat App</span>
-        </div>
-
-        <div class="space-x-4">
-          <NuxtLink to="/" class="text-white hover:underline">Home</NuxtLink>
-          <NuxtLink to="/createUser" class="text-white hover:underline">Create User</NuxtLink>
-          <NuxtLink to="/chat" class="text-white hover:underline">Chat</NuxtLink>
-        </div>
-      </div>
-    </nav>
-    <slot />
-</div>
-  </template>
-  
-  <script>
-
-  </script>
-  
-  <style scoped>
-  </style>
     <div>
-        <!-- content goes here -->
+        <nav class="bg-gray-800 p-4">
+            <div class="container mx-auto flex justify-between items-center">
+                <div class="flex items-center">
+                    <img src="/assets/logo.png" alt="Logo" class="h-8 mr-2" />
+                    <span class="text-white text-xl font-semibold">DD Chat App</span>
+                </div>
+
+                <div class="space-x-4">
+                    <NuxtLink to="/" class="text-white hover:underline">Home</NuxtLink>
+                    <NuxtLink to="/createUser" class="text-white hover:underline">Create User</NuxtLink>
+                    <NuxtLink to="/chat" class="text-white hover:underline">Chat</NuxtLink>
+                </div>
+            </div>
+        </nav>
         <slot />
     </div>
 </template>
+
 
 <script setup>
 
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,13 +1,29 @@
 <template>
-    <div>
-
+    <div class="max-w-5xl mx-auto border-solid border-2 mt-2 rounded-3xl p-3">
+        <div class="flex justify-between items-end ">
+            <div>
+                <UserChatList />
+                <UserChatList />
+                <UserChatList />
+                <UserChatList />
+                <UserChatList />
+                <UserChatList />
+            </div>
+            <div>
+                <MyChat />
+            </div>
+            
+        </div>
+        <div>
+            <ChatBox />
+        </div>
     </div>
+
 </template>
 
 <script setup>
 
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
