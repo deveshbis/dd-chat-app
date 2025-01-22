93949a03e0a9cb0e760bccc1fcb419dca93afc46
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-01-22 11:41:36 +0600

    Header completed (Responsive)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,18 @@
 <template>
-    <div>
-        <nav class="bg-gray-800 p-4">
-            <div class="container mx-auto flex justify-between items-center">
-                <div class="flex items-center">
-                    <NuxtLink to="/">
-                        <img src="/assets/logo.png" alt="Logo" class="h-8 mr-2">
-                    </NuxtLink>
-                    <span class="text-white text-xl font-semibold">DD Chat App</span>
-                </div>
+    <main>
+      <div>
+        <Navbar />
+      </div>
+     <div>
+      <slot />
+     </div>
+    </main>
 
-                <div class="space-x-4">
-                    <NuxtLink to="/" class="nbtn">Home</NuxtLink>
-                    <NuxtLink to="/createUser" class="nbtn">Create User</NuxtLink>
-                    <NuxtLink to="/chat" class="cbtn">Chat</NuxtLink>
-                    
-                </div>
-            </div>
-        </nav>
-        <slot />
-    </div>
 </template>
 
-
 <script setup>
-
+    import Navbar from '~/components/Shared/Navbar';
 </script>
 
 <style scoped>
-    
 </style>
