910b01043f16972a617bdc16f8389088dfae9f5d
Dihan <96198849+Rafeeuzzaman-Dihan@users.noreply.github.com>
2025-01-23 01:38:30 +0600

    State Management Done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  modules: [
+    '@pinia/nuxt', // Add Pinia module here
+  ],
+  
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
