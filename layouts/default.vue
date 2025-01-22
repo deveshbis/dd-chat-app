9ecaf94aff44ac4d24f299d365cfedbe2ffe521e
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-01-22 10:41:47 +0600

    Navbar Completed

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,14 +3,17 @@
         <nav class="bg-gray-800 p-4">
             <div class="container mx-auto flex justify-between items-center">
                 <div class="flex items-center">
-                    <img src="/assets/logo.png" alt="Logo" class="h-8 mr-2" />
+                    <NuxtLink to="/">
+                        <img src="/assets/logo.png" alt="Logo" class="h-8 mr-2">
+                    </NuxtLink>
                     <span class="text-white text-xl font-semibold">DD Chat App</span>
                 </div>
 
                 <div class="space-x-4">
-                    <NuxtLink to="/" class="text-white hover:underline">Home</NuxtLink>
-                    <NuxtLink to="/createUser" class="text-white hover:underline">Create User</NuxtLink>
-                    <NuxtLink to="/chat" class="text-white hover:underline">Chat</NuxtLink>
+                    <NuxtLink to="/" class="nbtn">Home</NuxtLink>
+                    <NuxtLink to="/createUser" class="nbtn">Create User</NuxtLink>
+                    <NuxtLink to="/chat" class="cbtn">Chat</NuxtLink>
+                    
                 </div>
             </div>
         </nav>
@@ -23,4 +26,6 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+    
+</style>
